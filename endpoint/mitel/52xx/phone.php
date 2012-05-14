1dd892bdff40d20e895ffd21b7c01dec1e25d209
root <root@pbx.local>
2012-05-13 21:13:01 -0400

    Changes to Mitel

--- endpoint/mitel/52xx/phone.php
+++ endpoint/mitel/52xx/phone.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Polycom SoundPoint In Production Modules Phone File
+ * Mitel 52xx Modules Phone File
  *
  * @author Andrew Nagy
  * @license MPL / GPLv2 / LGPL
  * @package Provisioner
  */
-class endpoint_mitel_53xx_phone extends endpoint_mitel_base {
+class endpoint_mitel_52xx_phone extends endpoint_mitel_base {
 
-    public $family_line = '53xx';
+    public $family_line = '52xx';
     public $dynamic_mapping = array(
 		'MN_$mac.cfg'=>array('MN_Generic.cfg','MN_$mac.cfg'),
 		'MN_Generic.cfg'=>'#This File is intentionally left blank',
